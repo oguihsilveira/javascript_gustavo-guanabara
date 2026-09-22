e2c2fb0ac6d14f3afe74a0e687a84d12116669e5
Guilherme Henrique <guilhermehsilveira.95@gmail.com>
2026-09-22 09:26:38 -0300

    chore: class 13 while and do while exercises done

---
+++ aula_13/ambiente.js
@@ -0,0 +1,20 @@
+var contador = 1
+do {
+    console.log(`Olá, tudo bem! Passo ${contador}.`)
+    contador++
+} while (contador <= 6)
+
+/* var contador = 1
+while (contador <= 6){
+    console.log(`Olá, tudo bem! Passo ${contador}.`)
+    contador++
+} */
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
