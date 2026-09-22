e25c01be617d83cea2f9d5eb8e77e6ed92d1af3a
Guilherme Henrique <guilhermehsilveira.95@gmail.com>
2026-09-22 13:15:06 -0300

    chore: complete class 15 on arrays

---
+++ aula_15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 6, 7, 8, 9, 2, 4, 2, 0]
+
+num.push(2)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}.`)
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
