ad059f804ad9c974fa6d9cbdc9028c1473f82146
Guilherme Henrique <guilhermehsilveira.95@gmail.com>
2026-09-23 10:47:14 -0300

    chore: class 16 functions complete

---
+++ aula_16/funcao_01.js
@@ -0,0 +1,10 @@
+function par_impar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+let res = par_impar(4)
+console.log(par_impar(223))
---
+++ aula_16/funcao_02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0) { // Se n1 ou n2 não forem passados, serão considerados 0.
+    return n1 + n2
+}
+console.log(soma(2, 5))
